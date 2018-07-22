006bb35a8b9a7f5ec6db5e12c5663e46acc4c8e7
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-22 13:10:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def position_taken?(board, index)
   # if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-  if (!board[index]) || (board[index].strip == "") #possible because of the short-circuit nature of Ruby 
+  if (!board[index]) || (board[index].strip == "") #possible because of the short-circuit nature of evaluation in Ruby
     false
   else
     true
